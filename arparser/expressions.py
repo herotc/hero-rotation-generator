4d31d4174ea0e2edb20ded1f8f299d2fe899fb5f
skasch <romain.mondoncancel@gmail.com>
2017-12-12 17:04:54 +0100

    Put resources in additional file

--- arparser/expressions.py
+++ arparser/expressions.py
@@ -5,9 +5,11 @@ Define the objects representing simc expressions.
 @author: skasch
 """
 
-from .lua import LuaNamed, LuaExpression, Method, Literal
+from .lua import LuaExpression, Method, Literal
 from .executions import Spell, Item
-from .druid import balance_astral_power_value
+from .resources import (Rune, AstralPower, HolyPower, Insanity, Pain, Focus,
+                        Maelstrom, Energy, ComboPoints, SoulShards, 
+                        ArcaneCharges, Chi, RunicPower, Fury, Mana)
 from .units import Pet
 from .constants import SPELL, BUFF, DEBUFF, BOOL, PET, BLOODLUST, RANGE
 
@@ -394,7 +396,7 @@ class Aura(Expires):
         self.object_ = self.spell
         self.method = Method('TickTime')
         self.args = []
-    
+
     def ticking(self):
         """
         Return the arguments for the expression {aura}.spell.ticking.
@@ -506,7 +508,7 @@ class ActionExpression(BuildExpression):
         Return the arguments for the expression action.spell.travel_time.
         """
         self.method = Method('TravelTime')
-    
+
     def max_charges(self):
         """
         Return the arguments for the expression action.spell.max_charges.
@@ -724,13 +726,13 @@ class Artifact(BuildExpression):
                              condition.condition_list[1])
         self.args = []
         super().__init__(call)
-    
+
     def rank(self):
         """
         Return the arguments for the expression artifact.spell.rank.
         """
         self.method = Method('ArtifactRank')
-    
+
     def enabled(self):
         """
         Return the arguments for the expression artifact.spell.enabled.
@@ -786,203 +788,6 @@ class SpellTargets(LuaExpression):
         super().__init__(object_, method, args, array=True)
 
 
-class Resource(BuildExpression):
-    """
-    Represent the expression for resource (mana, runic_power, etc) condition.
-    """
-
-    def __init__(self, condition, simc):
-        self.condition = condition
-        self.simc = Literal(simc, convert=True)
-        if len(condition.condition_list) > 1:
-            call = condition.condition_list[1]
-        else:
-            call = 'value'
-        self.object_ = condition.player_unit
-        self.args = []
-        super().__init__(call)
-
-    def value(self):
-        """
-        Return the arguments for the expression {resource}.
-        """
-        self.method = Method(f'{self.simc.print_lua()}')
-
-    def deficit(self):
-        """
-        Return the arguments for the expression {resource}.deficit.
-        """
-        self.method = Method(f'{self.simc.print_lua()}Deficit')
-
-    def pct(self):
-        """
-        Return the arguments for the expression {resource}.pct.
-        """
-        self.method = Method(f'{self.simc.print_lua()}Percentage')
-    
-    def regen(self):
-        """
-        Return the arguments for the expression {resource}.regen.
-        """
-        self.method = Method(f'{self.simc.print_lua()}Regen')
-    
-    def max(self):
-        """
-        Return the arguments for the expression {resource}.max.
-        """
-        self.method = Method(f'{self.simc.print_lua()}Max')
-
-
-class Rune(Resource):
-    """
-    Represent the expression for a rune. condition.
-    """
-
-    def __init__(self, condition):
-        if (len(condition.condition_list) > 1
-                and condition.condition_list[1][:-1] == 'time_to_'):
-            condition.condition_list.append(condition.condition_list[1][-1])
-            condition.condition_list[1] = 'time_to'
-        super().__init__(condition, 'rune')
-
-    def time_to(self):
-        """
-        Return the arguments for the expression rune.time_to_X.
-        """
-        self.method = Method('RuneTimeToX')
-        self.args = [Literal(self.condition.condition_list[2])]
-
-
-class AstralPower(Resource):
-    """
-    Represent the expression for a astral_power. condition.
-    """
-
-    def __init__(self, condition):
-        super().__init__(condition, 'astral_power')
-
-    @balance_astral_power_value
-    def value(self):
-        return super().value()
-
-
-class HolyPower(Resource):
-    """
-    Represent the expression for a runic_power. condition.
-    """
-
-    def __init__(self, condition):
-        super().__init__(condition, 'holy_power')
-
-
-class Insanity(Resource):
-    """
-    Represent the expression for a insanity. condition.
-    """
-
-    def __init__(self, condition):
-        super().__init__(condition, 'insanity')
-
-
-class Pain(Resource):
-    """
-    Represent the expression for a pain. condition.
-    """
-
-    def __init__(self, condition):
-        super().__init__(condition, 'pain')
-
-
-class Focus(Resource):
-    """
-    Represent the expression for a focus. condition.
-    """
-
-    def __init__(self, condition):
-        super().__init__(condition, 'focus')
-
-
-class Maelstrom(Resource):
-    """
-    Represent the expression for a maelstrom. condition.
-    """
-
-    def __init__(self, condition):
-        super().__init__(condition, 'maelstrom')
-
-
-class Energy(Resource):
-    """
-    Represent the expression for a energy. condition.
-    """
-
-    def __init__(self, condition):
-        super().__init__(condition, 'energy')
-
-
-class ComboPoints(Resource):
-    """
-    Represent the expression for a combo_points. condition.
-    """
-
-    def __init__(self, condition):
-        super().__init__(condition, 'combo_points')
-
-
-class SoulShards(Resource):
-    """
-    Represent the expression for a soul_shards. condition.
-    """
-
-    def __init__(self, condition):
-        super().__init__(condition, 'soul_shards')
-
-
-class ArcaneCharges(Resource):
-    """
-    Represent the expression for a arcane_charges. condition.
-    """
-
-    def __init__(self, condition):
-        super().__init__(condition, 'arcane_charges')
-
-
-class Chi(Resource):
-    """
-    Represent the expression for a chi. condition.
-    """
-
-    def __init__(self, condition):
-        super().__init__(condition, 'chi')
-
-
-class RunicPower(Resource):
-    """
-    Represent the expression for a runic_power. condition.
-    """
-
-    def __init__(self, condition):
-        super().__init__(condition, 'runic_power')
-
-
-class Fury(Resource):
-    """
-    Represent the expression for a fury. condition.
-    """
-
-    def __init__(self, condition):
-        super().__init__(condition, 'fury')
-
-
-class Mana(Resource):
-    """
-    Represent the expression for a mana. condition.
-    """
-
-    def __init__(self, condition):
-        super().__init__(condition, 'mana')
-
-
 class Debuff(BuildExpression, Aura):
     """
     Represent the expression for a debuff. condition.
@@ -1034,10 +839,10 @@ class Cooldown(BuildExpression, Expires):
         Return the arguments for the expression cooldown.spell.recharge_time.
         """
         self.method = Method('RechargeP')
-    
+
     def charges_fractional(self):
         """
-        Return the arguments for the expression 
+        Return the arguments for the expression
         cooldown.spell.charges_fractional.
         """
         self.method = Method('ChargesFractional')
---
+++ arparser/resources.py
@@ -0,0 +1,208 @@
+# -*- coding: utf-8 -*-
+"""
+Define the objects representing simc resources expressions.
+
+@author: skasch
+"""
+
+
+from .expressions import BuildExpression
+from .lua import Literal, Method
+from .druid import balance_astral_power_value
+
+
+class Resource(BuildExpression):
+    """
+    Represent the expression for resource (mana, runic_power, etc) condition.
+    """
+
+    def __init__(self, condition, simc):
+        self.condition = condition
+        self.simc = Literal(simc, convert=True)
+        if len(condition.condition_list) > 1:
+            call = condition.condition_list[1]
+        else:
+            call = 'value'
+        self.object_ = condition.player_unit
+        self.args = []
+        super().__init__(call)
+
+    def value(self):
+        """
+        Return the arguments for the expression {resource}.
+        """
+        self.method = Method(f'{self.simc.print_lua()}')
+
+    def deficit(self):
+        """
+        Return the arguments for the expression {resource}.deficit.
+        """
+        self.method = Method(f'{self.simc.print_lua()}Deficit')
+
+    def pct(self):
+        """
+        Return the arguments for the expression {resource}.pct.
+        """
+        self.method = Method(f'{self.simc.print_lua()}Percentage')
+
+    def regen(self):
+        """
+        Return the arguments for the expression {resource}.regen.
+        """
+        self.method = Method(f'{self.simc.print_lua()}Regen')
+
+    def max(self):
+        """
+        Return the arguments for the expression {resource}.max.
+        """
+        self.method = Method(f'{self.simc.print_lua()}Max')
+
+
+class Rune(Resource):
+    """
+    Represent the expression for a rune. condition.
+    """
+
+    def __init__(self, condition):
+        if (len(condition.condition_list) > 1
+                and condition.condition_list[1][:-1] == 'time_to_'):
+            condition.condition_list.append(condition.condition_list[1][-1])
+            condition.condition_list[1] = 'time_to'
+        super().__init__(condition, 'rune')
+
+    def time_to(self):
+        """
+        Return the arguments for the expression rune.time_to_X.
+        """
+        self.method = Method('RuneTimeToX')
+        self.args = [Literal(self.condition.condition_list[2])]
+
+
+class AstralPower(Resource):
+    """
+    Represent the expression for a astral_power. condition.
+    """
+
+    def __init__(self, condition):
+        super().__init__(condition, 'astral_power')
+
+    @balance_astral_power_value
+    def value(self):
+        return super().value()
+
+
+class HolyPower(Resource):
+    """
+    Represent the expression for a runic_power. condition.
+    """
+
+    def __init__(self, condition):
+        super().__init__(condition, 'holy_power')
+
+
+class Insanity(Resource):
+    """
+    Represent the expression for a insanity. condition.
+    """
+
+    def __init__(self, condition):
+        super().__init__(condition, 'insanity')
+
+
+class Pain(Resource):
+    """
+    Represent the expression for a pain. condition.
+    """
+
+    def __init__(self, condition):
+        super().__init__(condition, 'pain')
+
+
+class Focus(Resource):
+    """
+    Represent the expression for a focus. condition.
+    """
+
+    def __init__(self, condition):
+        super().__init__(condition, 'focus')
+
+
+class Maelstrom(Resource):
+    """
+    Represent the expression for a maelstrom. condition.
+    """
+
+    def __init__(self, condition):
+        super().__init__(condition, 'maelstrom')
+
+
+class Energy(Resource):
+    """
+    Represent the expression for a energy. condition.
+    """
+
+    def __init__(self, condition):
+        super().__init__(condition, 'energy')
+
+
+class ComboPoints(Resource):
+    """
+    Represent the expression for a combo_points. condition.
+    """
+
+    def __init__(self, condition):
+        super().__init__(condition, 'combo_points')
+
+
+class SoulShards(Resource):
+    """
+    Represent the expression for a soul_shards. condition.
+    """
+
+    def __init__(self, condition):
+        super().__init__(condition, 'soul_shards')
+
+
+class ArcaneCharges(Resource):
+    """
+    Represent the expression for a arcane_charges. condition.
+    """
+
+    def __init__(self, condition):
+        super().__init__(condition, 'arcane_charges')
+
+
+class Chi(Resource):
+    """
+    Represent the expression for a chi. condition.
+    """
+
+    def __init__(self, condition):
+        super().__init__(condition, 'chi')
+
+
+class RunicPower(Resource):
+    """
+    Represent the expression for a runic_power. condition.
+    """
+
+    def __init__(self, condition):
+        super().__init__(condition, 'runic_power')
+
+
+class Fury(Resource):
+    """
+    Represent the expression for a fury. condition.
+    """
+
+    def __init__(self, condition):
+        super().__init__(condition, 'fury')
+
+
+class Mana(Resource):
+    """
+    Represent the expression for a mana. condition.
+    """
+
+    def __init__(self, condition):
+        super().__init__(condition, 'mana')
