abe5e7321ecc8c97cce571503bfc480a5ce4d16e
Cilraaz <cilraaz@gmail.com>
2019-01-16 20:01:44 -0500

    [Hunter] Update BM/MM Spell IDs and APLs

--- hrgenerator/classes/hunter.py
+++ hrgenerator/classes/hunter.py
@@ -39,86 +39,98 @@ DEFAULT_RANGE = {
 SPELL_INFO = {
     HUNTER: {
         COMMON: {
-            'multishot':                    {SPELL:     2643,
-                                             RANGE:     40},
             'barrage':                      {SPELL:     120360,
-                                             RANGE:     40},
-            'binding_shot':                 {SPELL:     109248},
-            'volley':                       {SPELL:     194386},
+                                             RANGE:     40,
+                                             READY:     True},
+            'binding_shot':                 {SPELL:     109248,
+                                             CD:        True},
             'aspect_of_the_turtle':         {SPELL:     186265},
-            'exhilaration':                 {SPELL:     109304},
-            'aspect_of_the_cheetah':        {SPELL:     186257},
+            'exhilaration':                 {SPELL:     109304,
+                                             GCDAOGCD:  True},
+            'aspect_of_the_cheetah':        {SPELL:     186257,
+                                             CD:        True},
             'counter_shot':                 {SPELL:     147362},
             'disengage':                    {SPELL:     781},
             'freezing_trap':                {SPELL:     187650},
             'feign_death':                  {SPELL:     5384},
             'tar_trap':                     {SPELL:     187698},
-            'muzzle':                       {SPELL:     187707,
-                                             INTERRUPT: True},
-            # Legendaries
-            'parsels_tongue':               {BUFF:      248084},
-            'sephuzs_secret':               {BUFF:      208052},
-            # Azerite
-            'up_close_and_personal':        {SPELL:     278533},
-            'wilderness_survival':          {SPELL:     278532},
-            'blur_of_talons':               {SPELL:     277653,
-                                             BUFF:      277969}, # CHECK BUFF ID
-            'latent_poison':                {SPELL:     273283,
-                                             DEBUFF:    273286}, # CHECK DEBUFF ID
-            'venomous_fangs':               {SPELL:     274590},
+            'a_murder_of_crows':            {SPELL:     131894,
+                                             GCDAOGCD:  True},
+            'summon_pet':                   {SPELL:     883,
+                                             GCDAOGCD:  True},
         },
         BEAST_MASTERY: {
             'aspect_of_the_wild':           {SPELL:     193530,
-                                             BUFF:      193530},
+                                             BUFF:      193530,
+                                             GCDAOGCD:  True},
             'beast_cleave':                 {SPELL:     115939,
                                              BUFF:      118455,
                                              PET:       True},
             'bestial_wrath':                {SPELL:     19574,
-                                             BUFF:      19574},
+                                             BUFF:      19574,
+                                             GCDAOGCD:  True},
             'cobra_shot':                   {SPELL:     193455},
             'dire_beast':                   {SPELL:     120679,
                                              RANGE:     40},
             'kill_command':                 {SPELL:     34026},
-            'a_murder_of_crows':            {SPELL:     131894},
             'aspect_of_the_beast':          {SPELL:     191384},
             'bestial_ferocity':             {SPELL:     191413},
             'chimaera_shot':                {SPELL:     53209},
-            'dire_frenzy':                  {SPELL:     217200,
-                                             BUFF:      217200},
-            'dire_stable':                  {SPELL:     193532},
+            'barbed_shot':                  {SPELL:     217200},
+            'scent_of_blood':               {SPELL:     193532},
             'one_with_the_pack':            {SPELL:     199528},
-            'stampede':                     {SPELL:     201430},
-            'titans_thunder':               {SPELL:     207068},
+            'stampede':                     {SPELL:     201430,
+                                             GCDAOGCD:  True},
+            'multishot':                    {SPELL:     2643,
+                                             RANGE:     40},
+            'killer_instinct':              {SPELL:     273887},
+            'frenzy':                       {SPELL:     272790,
+                                             BUFF:      272790},
+            'spitting_cobra':               {SPELL:     194407,
+                                             GCDAOGCD:  True},
+            # Azerite Traits
+            'primal_instincts':             {SPELL:     279806,
+                                             BUFF:      279810}, # CHECK BUFF ID
         },
         MARKSMANSHIP: {
             'aimed_shot':                   {SPELL:     19434,
-                                             RANGE:     40},
+                                             RANGE:     40,
+                                             READY:     True},
             'arcane_shot':                  {SPELL:     185358},
             'bursting_shot':                {SPELL:     186387},
-            'hunters_mark':                 {DEBUFF:    185365},
-            'marked_shot':                  {SPELL:     185901,
-                                             RANGE:     40},
-            'marking_targets':              {BUFF:      223138},
-            'trueshot':                     {SPELL:     193526,
-                                             BUFF:      193526},
-            'vulnerability':                {DEBUFF:    187131},
-            'a_murder_of_crows':            {SPELL:     131894},
-            'black_arrow':                  {SPELL:     194599},
+            'hunters_mark':                 {SPELL:     257284,
+                                             DEBUFF:    257284},
+            'trueshot':                     {SPELL:     288613,
+                                             BUFF:      288613,
+                                             GCDAOGCD:  True},
             'explosive_shot':               {SPELL:     212431},
             'lock_and_load':                {BUFF:      194594},
-            'patient_sniper':               {SPELL:     234588},
             'piercing_shot':                {SPELL:     198670,
                                              RANGE:     40},
-            'sentinel':                     {SPELL:     206817},
-            'sidewinders':                  {SPELL:     214579},
             'trick_shot':                   {SPELL:     199522},
-            'windburst':                    {SPELL:     204147},
-            'bullseye':                     {BUFF:      204090},
-            'sentinels_sight':              {BUFF:      208913},
-            'critical_aimed':               {BUFF:      242243},
             'serpent_sting':                {SPELL:     271788,
                                              DEBUFF:    271788},
-            't20_2p_critical_aimed_damage': {BUFF:      242242},
+            'multishot':                    {SPELL:     257620,
+                                             RANGE:     40},
+            'double_tap':                   {SPELL:     260402},
+            'calling_the_shots':            {SPELL:     260404},
+            'streamline':                   {SPELL:     260367},
+            'rapid_fire':                   {SPELL:     257044},
+            'careful_aim':                  {SPELL:     260228},
+            'master_marksman':              {SPELL:     260309,
+                                             BUFF:      269576},
+            'precise_shots':                {SPELL:     260240,
+                                             BUFF:      260242},
+            'steady_focus':                 {SPELL:     193533},
+            'steady_shot':                  {SPELL:     56641},
+            'trick_shots':                  {BUFF:      257622},
+            'volley':                       {SPELL:     194386},
+            # Azerite Traits
+            'unerring_vision':              {SPELL:     274444,
+                                             BUFF:      274446},
+            'surging_shots':                {SPELL:     287707},
+            'focused_fire':                 {SPELL:     278531},
+            'in_the_rhythm':                {SPELL:     264198},
         },
         SURVIVAL: {
             'aspect_of_the_eagle':          {SPELL:     186289,
@@ -137,7 +149,6 @@ SPELL_INFO = {
             'mongoose_fury':                {BUFF:      259388},
             'raptor_strike':                {SPELL:     186270,
                                              READY:     True},
-            'a_murder_of_crows':            {SPELL:     131894},
             'butchery':                     {SPELL:     212436,
                                              GCDAOGCD:  True},
             'serpent_sting':                {SPELL:     259491,
@@ -146,8 +157,6 @@ SPELL_INFO = {
             'steel_trap':                   {SPELL:     162488,
                                              DEBUFF:    162487,
                                              OPENER:    True},
-            'summon_pet':                   {SPELL:     883,
-                                             GCDAOGCD:  True},
             'coordinated_assault':          {SPELL:     266779,
                                              BUFF:      266779,
                                              GCDAOGCD:  True,
@@ -168,6 +177,16 @@ SPELL_INFO = {
             'birds_of_prey':                {SPELL:     260331},
             'alpha_predator':               {SPELL:     269737}, 
             'bloodseeker':                  {DEBUFF:    259277},
+            'muzzle':                       {SPELL:     187707,
+                                             INTERRUPT: True},
+            # Azerite Traits
+            'up_close_and_personal':        {SPELL:     278533},
+            'wilderness_survival':          {SPELL:     278532},
+            'blur_of_talons':               {SPELL:     277653,
+                                             BUFF:      277969}, # CHECK BUFF ID
+            'latent_poison':                {SPELL:     273283,
+                                             DEBUFF:    273286}, # CHECK DEBUFF ID
+            'venomous_fangs':               {SPELL:     274590},
         },
     },
 }
@@ -191,77 +210,12 @@ CLASS_FUNCTIONS = {
         BEAST_MASTERY: [
         ],
         MARKSMANSHIP: [
-            'TargetDebuffRemainsP',
-            'TargetDebuffP',
         ],
         SURVIVAL: [
             'SurvivalPreAPLSetup',
         ],
     },
 }
-
-
-def marksmanship_lowest_vuln_within(fun):
-    """
-    Call TargetDebuffRemainsP function for lowest_vuln_within.
-    """
-    from ..objects.executions import Spell
-    from ..objects.lua import LuaExpression, Method
-
-
-    def lowest_vuln_within(self):
-        """
-        Return the condition when the prefix is lowest_vuln_within.
-        """
-        if self.player_unit.spec.simc == MARKSMANSHIP:
-            object_ = None
-            method = Method('TargetDebuffRemainsP')
-            args = [Spell(self.parent_action, 'vulnerability', type_=DEBUFF)]
-            return LuaExpression(object_, method, args)
-        else:
-            fun(self)
-
-    return lowest_vuln_within
-
-
-def marksmanship_debuff_remains(fun):
-    """
-    Call TargetDebuffRemainsP function for lowest_vuln_within.
-    """
-    from ..objects.lua import Method
-
-    def remains(self):
-        """
-        Return the arguments for the expression debuff.spell.remains.
-        """
-        if self.condition.player_unit.spec.simc == MARKSMANSHIP:
-            self.object_ = None
-            self.method = Method('TargetDebuffRemainsP')
-            self.args = [self.spell]
-        else:
-            fun(self)
-
-    return remains
-
-
-def marksmanship_debuff_ready(fun):
-    """
-    Call TargetDebuffRemainsP function for lowest_vuln_within.
-    """
-    from ..objects.lua import Method
-
-    def ready(self):
-        """
-        Return the arguments for the expression debuff.spell.up/ready.
-        """
-        if self.condition.player_unit.spec.simc == MARKSMANSHIP:
-            self.object_ = None
-            self.method = Method('TargetDebuffP', type_=BOOL)
-            self.args = [self.spell]
-        else:
-            fun(self)
-
-    return ready
 
 def survival_next_wi_bomb(fun):
 
@@ -298,21 +252,6 @@ DECORATORS = {
     HUNTER: [
         {
             'class_name': 'Expression',
-            'method': 'lowest_vuln_within',
-            'decorator': marksmanship_lowest_vuln_within,
-        },
-        {
-            'class_name': 'Debuff',
-            'method': 'remains',
-            'decorator': marksmanship_debuff_remains,
-        },
-        {
-            'class_name': 'Debuff',
-            'method': 'ready',
-            'decorator': marksmanship_debuff_ready,
-        },
-        {
-            'class_name': 'Expression',
             'method': 'next_wi_bomb',
             'decorator': survival_next_wi_bomb,
         },
@@ -335,8 +274,8 @@ TEMPLATES = {
                         '  S.RaptorStrike = CurrentRaptorStrike()\n'
                         '  S.MongooseBite = CurrentMongooseBite()\n'
                         '{action_lists}\n'
-                        '{precombat_call}\n'
                         '  if Everyone.TargetIsValid() then\n'
+                        '{precombat_call}\n'
                         '{main_actions}\n'
                         '  end\n'
                         'end\n'
